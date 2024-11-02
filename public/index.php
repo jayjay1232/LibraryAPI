1fca68a2addf7f2e604996f4c592865b82dd86a6
YARANON_JAY <70697378+jayjay1232@users.noreply.github.com>
2024-11-02 20:35:12 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ $app = new \Slim\App;
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "librarydb";
+$dbname = "library";
 
 // Function to generate a new access token
 function generateAccessToken() {
